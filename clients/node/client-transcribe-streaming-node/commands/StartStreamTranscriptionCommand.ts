77c7f273536b0cbb81c14889c6ea3dbbe2c70ed6
Chase Coalwell <chasecoa@amazon.com>
2019-10-08 15:50:50 -0700

    feat: add command

---
+++ clients/node/client-transcribe-streaming-node/commands/StartStreamTranscriptionCommand.ts
@@ -0,0 +1,61 @@
+import { Command } from "@aws-sdk/smithy-client";
+import { serializerPlugin } from "@aws-sdk/middleware-serializer";
+import { deserializerPlugin } from "@aws-sdk/middleware-deserializer";
+import * as __aws_sdk_types from "@aws-sdk/types";
+import { TranscribeStreamingResolvedConfiguration } from "../TranscribeStreamingConfiguration";
+import { HttpRequest } from "@aws-sdk/protocol-http";
+import {
+  startStreamTranscriptionSerializer,
+  startStreamTranscriptionDeserializer
+} from "../protocol/StartStreamTranscription";
+import { FinalizeHandlerArguments, MiddlewareStack } from "@aws-sdk/types";
+
+/**
+ * To remove this when move to Smithy model
+ */
+type StartStreamTranscriptionInput = any;
+type StartStreamTranscriptionOutput = any;
+type InputTypesUnion = any;
+type OutputTypesUnion = any;
+
+export class StartStreamTranscriptionCommand extends Command<
+  StartStreamTranscriptionInput,
+  StartStreamTranscriptionOutput
+> {
+  constructor(readonly input: StartStreamTranscriptionInput) {
+    super();
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<InputTypesUnion, OutputTypesUnion>,
+    configuration: TranscribeStreamingResolvedConfiguration,
+    options?: __aws_sdk_types.HttpOptions
+  ): __aws_sdk_types.Handler<
+    StartStreamTranscriptionInput,
+    StartStreamTranscriptionOutput
+  > {
+    const { httpHandler } = configuration;
+
+    this.use(
+      serializerPlugin(configuration, startStreamTranscriptionSerializer)
+    );
+    this.use(
+      deserializerPlugin<StartStreamTranscriptionOutput>(
+        configuration,
+        startStreamTranscriptionDeserializer
+      )
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: __aws_sdk_types.HandlerExecutionContext = {
+      logger: {} as any
+    };
+
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        httpHandler.handle(request.request as HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+}
