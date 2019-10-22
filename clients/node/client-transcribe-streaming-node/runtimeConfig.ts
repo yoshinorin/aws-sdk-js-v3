4bc6aa2392422172375d074b3a8fa045ca30054d
Chase Coalwell <chasecoa@amazon.com>
2019-10-22 14:29:46 -0700

    feat: use Http2Handler for node

--- clients/node/client-transcribe-streaming-node/runtimeConfig.ts
+++ clients/node/client-transcribe-streaming-node/runtimeConfig.ts
@@ -1,19 +1,11 @@
 import { defaultProvider as credentialDefaultProvider } from "@aws-sdk/credential-provider-node";
 import { Hash } from "@aws-sdk/hash-node";
-<<<<<<< HEAD
 import { NodeHttp2Handler } from "@aws-sdk/node-http-handler";
-=======
-import { NodeHttpHandler } from "@aws-sdk/node-http-handler";
->>>>>>> feat: update plugin config
 import { defaultProvider as regionDefaultProvider } from "@aws-sdk/region-provider";
 import { parseUrl } from "@aws-sdk/url-parser-node";
 import { calculateBodyLength } from "@aws-sdk/util-body-length-node";
 import { streamCollector } from "@aws-sdk/stream-collector-node";
-<<<<<<< HEAD
 import { JsonProtocol } from "@aws-sdk/protocol-json";
-=======
-import { RestJsonProtocol } from "@aws-sdk/protocol-rest-json";
->>>>>>> feat: update plugin config
 import { fromUtf8, toUtf8 } from "@aws-sdk/util-utf8-node";
 import { fromBase64, toBase64 } from "@aws-sdk/util-base64-node";
 import { defaultUserAgent } from "@aws-sdk/util-user-agent-node";
@@ -23,17 +15,10 @@ import { TranscribeStreamingRuntimeDependencies } from "./TranscribeStreamingCon
 export const TranscribeStreamingRuntimeConfiguration: Required<
   TranscribeStreamingRuntimeDependencies
 > = {
-<<<<<<< HEAD
   protocolDefaultProvider: handler => new JsonProtocol(handler),
   signingName: "transcribestreaming",
   service: "transcribestreaming",
   httpHandler: new NodeHttp2Handler(),
-=======
-  protocolDefaultProvider: handler => new RestJsonProtocol(handler),
-  signingName: "transcribe-streaming",
-  service: "transcribe-streaming",
-  httpHandler: new NodeHttpHandler(),
->>>>>>> feat: update plugin config
   sha256: Hash.bind(null, "sha256"),
   credentialDefaultProvider,
   regionDefaultProvider,
